e257e546eed75db6239aab81b31cdfbdeeea4640
PedroAntonioVS <pedroantoniodev@gmail.com>
2024-07-04 23:27:31 -0300

    Adicionando pagina de detail

---
+++ detail.js
@@ -0,0 +1,26 @@
+//funcao asincrona interligado com o html (body on load)
+async function main() { 
+    // url da api
+    const api = 'https://rickandmortyapi.com/api/character/'+getId();
+    // responce chama api com o funcao fetch 
+    const response = await fetch(api);
+    // data recebe a resposta da api e response trasforma em json
+    const data = await response.json();
+    const imgDiv = document.getElementById("img")
+    const nameDiv = document.getElementById("name")
+    const statusDiv = document.getElementById("status")
+    const typeDiv = document.getElementById("type")
+    const genderDiv = document.getElementById("gender")
+    const originDiv = document.getElementById("origin")
+    nameDiv.innerHTML =`name: ${data.name}`
+    statusDiv.innerHTML = `status: ${data.status}`
+    typeDiv.innerHTML = `Type: ${data.type}`
+    genderDiv.innerHTML = `Gender: ${data.gender}`
+    originDiv.innerHTML = `Origin: ${data.origin.name}`
+    imgDiv.src = `https://rickandmortyapi.com/api/character/avatar/${getId()}.jpeg`
+
+}
+function getId() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get("id");
+}
--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-async function main() {
+//funcao asincrona interligado com o html (body on load)
+async function main() { 
+    // url da api
     const api = 'https://rickandmortyapi.com/api/character';
+    // responce chama api com o funcao fetch 
     const response = await fetch(api);
+    // data recebe a resposta da api e response trasforma em json
     const data = await response.json();
+    // obtem os dados da lista results na api
     const characters = data.results;
-
+    // characteresContainer pega o elemento da div (characteres)
     const charactersContainer = document.getElementById('characters');
+    // para cada character dentro de characters 
     for(character of characters){
+        // cria uma div dentro da div (characters)
         const characterDiv = document.createElement('div');
+        //adiciona uma classe chamada (character)
         characterDiv.classList.add('character');
+        // troca o html interno da div character
         characterDiv.innerHTML = `
+        <a href = detail.html?id=${character.id} >
+
             <img src="${character.image}" alt="${character.name}">
             <p>${character.name}</p>
+        </a>
         `;
+        // charactersContainer adiciona (characterDiv) como filho
         charactersContainer.appendChild(characterDiv);
     };
 };
